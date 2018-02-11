610e3d91693e2c09a4ad1ffbc5f2e898515aa63b
Windows <gersoncalienes@gmail.com>
2018-02-10 22:16:43 -0500

    Finished with the promises, now moving on to the async-await version

--- app-promises.js
+++ app-promises.js
@@ -11,7 +11,23 @@ const users = [
   }
 ];
 
-const grades = [];
+const grades = [
+  {
+    id: 1,
+    schoolId: 101,
+    grade: 86
+  },
+  {
+    id: 2,
+    schoolId: 999,
+    grade: 100
+  },
+  {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+  }
+];
 
 const getUser = id => {
   return new Promise((resolve, reject) => {
@@ -26,10 +42,51 @@ const getUser = id => {
   });
 };
 
-getUser(2)
-  .then(user => {
-    console.log(user);
+const getGrades = schoolId => {
+  return new Promise(function(resolve, reject) {
+    resolve(grades.filter(grade => grade.schoolId === schoolId));
+  });
+};
+
+//Andrew has an 83% in the class
+const getStatus = userId => {
+  let user;
+  return getUser(userId)
+    .then(tempUser => {
+      user = tempUser;
+      return getGrades(user.schoolId);
+    })
+    .then(grades => {
+      let average = 0;
+      if (grades.length > 0) {
+        average =
+          grades.map(grade => grade.grade).reduce((a, b) => a + b) /
+          grades.length;
+      }
+      return `${user.name} has a ${average}% in the class.`;
+    });
+};
+
+getStatus(1)
+  .then(status => {
+    console.log(status);
   })
   .catch(e => {
     console.log(e);
   });
+
+// getGrades(101)
+//   .then(grades => {
+//     console.log(grades);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+// getUser(2)
+//   .then(user => {
+//     console.log(user);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
